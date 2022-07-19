95b5e9d0191507e2169c019339901cbc2bbb15fa
IMontano08 <101348771+IMontano08@users.noreply.github.com>
2022-07-19 15:37:48 -0500

    completed code
    can now randomly pick people

--- randomPerson.py
+++ randomPerson.py
@@ -2,52 +2,60 @@ from operator import truediv
 import random
 
 randomName = " "
-
+peopleInRoom = []
 def main():
-    list()
     randomPick()
     loop1()
     
 
                         
 def list():
-    peopleInRoom = []
-    n = int(input("Enter number of people in office: \n"))
+    while True:
+        try:
+            n = int(input("Enter number of people in office: \n"))
+        except ValueError:
+            print("Make sure you only input numbers")
+            continue
+        break 
+    
     print("Now enter their names (press enter after every name)")
     for i in range(0,n):
         people = str(input())
-        list.append(people)
-    print("\nYour list is: \n " + str(list))
+        peopleInRoom.append(people)
+    print("\nYour list is: \n ")
     
 def randomPick():
+    print(peopleInRoom)
     print("Now picking one at random\n")
-    randomName = random.choice(list)
+    global randomName 
+    randomName = random.choice(peopleInRoom)
     print(str(randomName) +"\n") 
     
 def remove():
-    list.remove(randomName)
+    print(peopleInRoom)
+    global randomName
+    print("\nRemoving " + randomName  + " from list... \n")
+    peopleInRoom.remove(randomName)
+
+
+def finalMsg(): 
+    print("\nHave fun at Lunch\n")
 
 def loop1():
-    loopCond = True
-    while loopCond == True:
-        x = input("Have another person pick? (Y/N)")
-        x = x.upper()
-        if x == "Y" | "YES":
+    loopCond = True;
+
+    while loopCond == True and len(peopleInRoom) >= 2:
+        
+        x = input("Have another person pick? (Y/N)\n")
+        if x.lower() == "y" or x.lower() == "yes":
             remove()
             randomPick()
-            
-        
-        
-
-def loop2():
-
-def loop3(): 
+        elif x.lower() == "n" or x.lower() == "no":
+            loopCond = False
+            finalMsg()
+        else:
+            print("Please select a valid option")
+    finalMsg()
     
-def finalMsg():          
-            
-        
-        
-        
-    
-    
+list()    
 main()
